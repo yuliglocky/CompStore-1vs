c62f6ffa79737f4f90ed1e7e927ea4b4b08e64b5
yuliglocky <102256366+yuliglocky@users.noreply.github.com>
2025-04-28 12:03:19 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true, 
   firebase: {
-    apiKey: "AIzaSyB7lh0Qp060Fsl6CXloDqhIMj_k_kWQ1qc",
-    authDomain: "compstore-1bd93.firebaseapp.com",
-    projectId: "compstore-1bd93",
-    storageBucket: "compstore-1bd93.appspot.com",
-    messagingSenderId: "1015709074208",
-    appId: "1:1015709074208:web:619efed3c921d251990d45",
-    measurementId: "G-182DXNTB01"
+    apiKey: "tu api key",
+    authDomain: "tu dominio",
+    projectId: "tu id",
+    storageBucket: "tu storage",
+    messagingSenderId: "1010101010",
+    appId: "tu app id",
+    measurementId: "tu measurement id "
   }
 };
+
